cdcc5714245a373e50531990b4a66cd6aefc90e6
uFNet1 <91597674+uFNet1@users.noreply.github.com>
2025-08-25 18:36:35 +0300

    wip add cookies

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,22 @@
 import { useState } from "react";
 import Settings from "./Settings";
+import Cookies from "js-cookie";
+
+const months = [
+  "",
+  "Січень",
+  "Лютий",
+  "Березень",
+  "Квітень",
+  "Травень",
+  "Червень",
+  "Липень",
+  "Серпень",
+  "Вересень",
+  "Жовтень",
+  "Листопад",
+  "Грудень",
+];
 
 function getCurrentDate() {
   const today = new Date();
@@ -14,48 +31,77 @@ function daysInMonth(month: number, year: number) {
 function getFirstWeekDay(month: number, year: number) {
   return new Date(year, month - 1, 1).getDay();
 }
+function mod(n: number, m: number) {
+  return ((n % m) + m) % m;
+}
 export default function Calendar() {
   const [isSelectingDay, setIsSelectingDay] = useState(false);
   const [selectedDay, setSelectedDay] = useState(null);
-  const [workDays, setWorkDays] = useState(null);
-  const [restDays, setRestDays] = useState(null);
+  const [formData, setFormData] = useState(["", ""]);
+  const [isFormFilled, setIsFormFilled] = useState(false);
+  const [showOnly, setShowOnly] = useState(0);
+  // const [remainingToSelect, setRemainingToSelect] = useState(0);
+  // const [typeToSelect, setTypeToSelect] = useState(0);
+
+  const handleChange = (value) => {
+    console.log("asd");
+    const newData = formData.map((c, index) => {
+      return value[index];
+    });
+    setFormData(newData);
+    if (newData[0] !== "" && newData[1] !== "") {
+      setIsFormFilled((prev) => true);
+      Cookies.set("schedule", value);
+    }
+  };
+  const handleRadioButtons = (value) => {
+    setShowOnly(value);
+  };
   function calculateDays(month: number, year: number) {
     const days = [];
     let weeks = [];
     let countDays = 0;
+    let typeToSelect = 0;
     const firstWeekDay = getFirstWeekDay(month, year);
     const totalDays = daysInMonth(month, year);
+
+    //start listing blank days
     if (firstWeekDay - 1 !== 0) {
       for (let index = 0; index < firstWeekDay - 1; index++) {
         weeks.push(<td key={year + month + index}>&nbsp;</td>);
         countDays++;
       }
     }
+    //start listing actual days
     for (let index = 0; index < totalDays; index++) {
+      //reset days on 7
       if (countDays === 7) {
         days.push(weeks);
         weeks = [];
         countDays = 0;
       }
-      const thisNumber = index + 1;
-      weeks.push(
-        <td
-          key={month + index}
-          onClick={(el) => {
-            if (isSelectingDay) {
-              setSelectedDay(el.target.textContent);
-              setIsSelectingDay(false);
-            }
-          }}
-          className={`cursor-pointer calendar-button px-2 py-1 rounded-lg ${
-            isSelectingDay
-              ? `animate-pulse bg-gray-600 hover:scale-105 hover:outline hover:outline-amber-200`
-              : ``
-          } ${Number(selectedDay) === thisNumber ? `bg-gray-600` : ``}`}
-        >
-          {thisNumber}
-        </td>
-      );
+      const thisDay = index + 1;
+      let currentDayType = null;
+
+      //calculate which days are work and which are rest
+      const workDays = Number(formData[0]);
+      const restDays = Number(formData[1]);
+      const totaBothDays = workDays + restDays;
+      const isAllowedToCalculate = isFormFilled ? (selectedDay !== null ? true : false) : false;
+      if (selectedDay) {
+        //current day type = ((This day) - (Day selected by user)) Mod (Sum of workDays + restDays)
+        currentDayType = mod(thisDay - Number(selectedDay), totaBothDays);
+        if (currentDayType >= 0 && currentDayType < workDays) {
+          typeToSelect = 0; //work day
+        } else if (currentDayType >= workDays && currentDayType < totaBothDays) {
+          typeToSelect = 1;
+        }
+      }
+
+      // if (selectedDay && thisDay < selectedDay) continue;
+      const isToday = thisDay === Number(date.day);
+      weeks.push(appendDaysToArr(month + index, thisDay, isToday, typeToSelect, isAllowedToCalculate));
+
       countDays++;
       if (index === totalDays - 1) {
         days.push(weeks);
@@ -63,18 +109,65 @@ export default function Calendar() {
     }
     return days;
   }
+  function appendDaysToArr(
+    key: number,
+    thisDay: number,
+    isToday: boolean,
+    typeToSelect: number,
+    isAllowedToCalculate: boolean
+  ) {
+    console.log(showOnly);
+    return (
+      <td
+        key={key}
+        onClick={(el) => {
+          if (isSelectingDay) {
+            setSelectedDay(el.target.textContent);
+            setIsSelectingDay(false);
+          }
+        }}
+        className={`cursor-pointer calendar-button px-2 py-1 rounded-lg 
+        
+        ${isSelectingDay ? `animate-pulse bg-gray-600 hover:scale-105 hover:outline hover:outline-amber-200` : ``}
+        ${Number(selectedDay) === thisDay ? `bg-gray-600` : ``}
+        
+        ${isToday && `text-white font-bold outline outline-emerald-200`}
+        ${
+          isAllowedToCalculate !== false
+            ? typeToSelect === 0
+              ? showOnly === 0 || showOnly === 1
+                ? `bg-red-700`
+                : null
+              : showOnly === 0 || showOnly === 2
+              ? `bg-green-700`
+              : null
+            : null
+        }
+        `}
+      >
+        {thisDay}
+      </td>
+    );
+  }
   const date = getCurrentDate();
   const days = calculateDays(Number(date.month), date.year);
   return (
     <>
       <Settings
+        onSetDays={handleChange}
         isSelectingDay={isSelectingDay}
         onSelectDay={() => setIsSelectingDay((prev) => !prev)}
+        isDaySelected={selectedDay}
+        isFormFilled={isFormFilled}
+        onSetShowType={handleRadioButtons}
       />
       <div>
         <button type="button" className="px-4 py-2 mt-4">
           {date.year}
         </button>
+        <p>
+          {date.month} - {months[Number(date.month)]}
+        </p>
       </div>
       <table className="border-separate border-spacing-3 mx-auto">
         <thead>
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,40 +1,130 @@
-export default function Settings({ isSelectingDay, onSelectDay }) {
-  const inputStyle = `w-8 h-8 bg-white rounded-lg text-black`;
+import { useState } from "react";
+import Cookies from "js-cookie";
+
+export default function Settings({
+  isSelectingDay,
+  onSelectDay,
+  onSetDays,
+  isDaySelected,
+  isFormFilled,
+  onSetShowType,
+}) {
+  const [workDays, setWorkDays] = useState<number | null>(null);
+  const [restDays, setRestDays] = useState<number | null>(null);
+  const [isWarning, setIsWarning] = useState(false);
+
+  function getScheduleCookie(type: number) {
+    const cookie = Cookies.get("schedule");
+    if (cookie) {
+      const cookieArr = cookie.split(",");
+      if (cookieArr[type]) {
+        if (type === 1) {
+          console.log("ase");
+          setWorkDays(Number(cookieArr[type]));
+          setRestDays(Number(cookieArr[type]));
+        }
+        return Number(cookieArr[type]);
+      }
+    }
+    return undefined;
+  }
+
+  const submitDays = () => {
+    console.log(workDays);
+    if (workDays && restDays) {
+      onSetDays([workDays, restDays]);
+      setIsWarning(false);
+    } else {
+      setIsWarning(true);
+      alert("Помилка: Не всі поля графіку введені!");
+    }
+  };
+  const setShowType = (value) => {
+    onSetShowType(value);
+  };
+  const inputStyle = (dataType: string | null) => {
+    return `w-8 h-8 bg-white rounded-lg text-black pl-2 text-center ${
+      isWarning && dataType === null ? `outline-3 outline-red-500` : null
+    }`;
+  };
+  const tickStyle = (dataType: boolean | null) => {
+    return `${!dataType ? `before:content-['❌']` : `before:content-['✔']`} before:mr-3`;
+  };
   return (
     <>
-      {isSelectingDay ? console.log("test") : console.log("ber")}
-      <fieldset className="border border-gray-600">
+      <fieldset className="border border-gray-600 flex flex-col items-start">
         <legend className="mr-auto ml-4 px-3">Налаштування:</legend>
-        <div className="flex flex-row gap-4 my-2 items-center mx-4">
+        <div className="flex flex-row gap-4 my-2 items-center ml-5">
           {/* <input type="checkbox" name="" id="" /> */}
-          <p className="">Графік:</p>
+          <p className={tickStyle(isFormFilled)}>Графік:</p>
           <input
-            onInput={(e) => (e.target.value = e.target.value.slice(0, 1))}
+            onInput={(e) => {
+              (e.target as HTMLInputElement).value = (e.target as HTMLInputElement).value.slice(0, 1);
+              setWorkDays((e.target as HTMLInputElement).value);
+            }}
             type="number"
             name="workDayFirst"
             id="workDayFirst"
-            className={inputStyle}
+            className={inputStyle(workDays)}
+            value={getScheduleCookie(0)}
           />
           <p>x</p>
           <input
-            onInput={(e) => (e.target.value = e.target.value.slice(0, 1))}
+            onInput={(e) => {
+              e.target.value = e.target.value.slice(0, 1);
+              setRestDays(e.target.value);
+            }}
             maxLength={1}
             type="number"
             name="workDaySecond"
             id="workDaySecond"
-            className={inputStyle}
+            className={inputStyle(restDays)}
+            value={getScheduleCookie(1)}
           />
-          <button type="button" className="w-8 h-8">
+          <button onClick={submitDays} type="button" className="w-8 h-8">
             ✔
           </button>
         </div>
-        <button
-          type="button"
-          className={`px-4 py-2 my-4 `}
-          onClick={onSelectDay}
-        >
-          Обрати перший робочий день
-        </button>
+        <span className={`${tickStyle(isDaySelected)} ml-5`}>
+          <button type="button" className={`px-4 py-2 my-4`} onClick={onSelectDay}>
+            Обрати перший роб. день
+          </button>
+        </span>
+        <hr className="border border-gray-300 w-2/3 mx-auto my-3" />
+        <div className="ml-5 mb-2">
+          <input
+            className="mr-3 w-4 h-4"
+            type="radio"
+            name="showOnly"
+            id="showAll"
+            value="all"
+            onClick={() => setShowType(0)}
+            defaultChecked={true}
+          />
+          <label htmlFor="showAll">Всі</label>
+        </div>
+        <div className="ml-5 mb-2">
+          <input
+            className="mr-3 w-4 h-4 accent-green-700"
+            type="radio"
+            name="showOnly"
+            id="showOnlyWork"
+            value="onlyWork"
+            onClick={() => setShowType(1)}
+          />
+          <label htmlFor="showOnlyWork">Тільки робочі</label>
+        </div>
+        <div className="ml-5 mb-3">
+          <input
+            className="mr-3 w-4 h-4 accent-red-700"
+            type="radio"
+            name="showOnly"
+            id="showOnlyRest"
+            value="onlyRest"
+            onClick={() => setShowType(2)}
+          />
+          <label htmlFor="showOnlyRest">Тільки вихідні</label>
+        </div>
       </fieldset>
     </>
   );
